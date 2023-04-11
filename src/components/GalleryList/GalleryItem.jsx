9f41e9fc86b38c2fdf1e65fa72a9bc493b870fb5
Sam Gossie <sjg3609@gmail.com>
2023-04-10 20:32:38 -0500

    Attempting the conditional rendering for the image description

--- src/components/GalleryList/GalleryItem.jsx
+++ src/components/GalleryList/GalleryItem.jsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
+import { useState } from 'react';
 
 function GalleryItem({images, galleryList, fetchGallery}) {
+
+    const [toggle, setToggle] = useState(false);
 
     const updateLikes = (e) => {
         axios.put(`gallery/like/${images.id}`).then((response) => {
@@ -12,9 +15,21 @@ function GalleryItem({images, galleryList, fetchGallery}) {
         });
     }
 
+    const showDescription = () => {
+        // testing to see if I can use onClick on an img tag
+        console.log('Does this work???');
+        if (toggle === true) {
+            return (
+                <div style={{width: 200, height: 200, color: 'gray'}}>
+                    {images.description}
+                </div>
+            );
+        }
+    }
+
     return (
         <div className="galleryItem">
-        { <img key={images.id} src={images.path}/> }
+        { <img key={images.id} src={images.path} onClick={showDescription}/> }
         <br/>
         <button onClick={(e) => updateLikes(e)}>Like</button>  {images.likes} people like this!
         <br/>
